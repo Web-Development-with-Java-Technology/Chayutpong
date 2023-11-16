72ece69d3f096bec1d9263066e5af3c82411fee2
Chayutpong <101621119+FirstSuffering@users.noreply.github.com>
2023-11-16 11:38:31 +0700

    Delete MyApp directory

--- MyApp/src/SumFor.java
+++
@@ -1,7 +0,0 @@
-public class SumFor {
-    public static void main(String[] args) {
-        int num=0;
-        for (int i=1; i<=10; i++) { num+=i; }
-        System.out.println("Sum 1-10 = "+num);
-    }
-}
